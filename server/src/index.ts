b43cb1c94ae08b131dc227d180da9af5ca561db7
Ansh Jain <anshjain9159@gmail.com>
2024-08-21 13:00:47 +0530

    chore: discussion page integrated

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,10 +25,11 @@ const app = express();
 app.use(
   cors({
     origin: "*",
-    methods: ["GET", "POST"],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS',],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 

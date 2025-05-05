8ae7a401ceb426f80f6226e4d08783fac5050312
blood <chayanmann09@gmail.com>
2025-05-06 00:06:18 +0530

    vercel url

--- server/src/index.ts
+++ server/src/index.ts
@@ -26,14 +26,26 @@ dotenv.config({
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://byte-progmatic.vercel.app/" 
+];
+
 app.use(
   cors({
-    origin:"http://localhost:5173",
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS',],
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true,  // Allow cookies in cross-origin requests
+    credentials: true,
   })
 );
+
 
 // Use cookie-parser middleware to parse cookies
 app.use(cookieParser());
